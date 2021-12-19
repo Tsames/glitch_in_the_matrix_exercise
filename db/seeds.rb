de2939b5d9516d041a830ed5aa70456b0bdb0a3d
tom ames <tsames1@gmail.com>
2021-12-18 17:25:03 -0800

    Commit 9: Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "Neo is a fictional character and the protagonist of The Matrix franchise, created by the Wachowskis."},
+	{ name: "Trinity", description: "Morpheus is a fictional character in The Matrix franchise." },
+	{ name: "Morpheus", description: "She is portrayed by Carrie-Anne Moss in the films." },
+	{ name: "Agent Smith", description: "He was portrayed by Hugo Weaving in the first trilogy of films and voiced by Christopher Corey Smith in The Matrix: Path of Neo." },
 ])
